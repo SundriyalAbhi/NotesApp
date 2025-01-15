d5ff142bdcb9ba0bdea603390ef3a88da3fbdfe2
AbhishekSundriyalprocoder <sundriyalabhishek98@gmail.com>
2025-01-15 21:47:15 +0530

    Utils update

--- client/src/Utils/Utils.js
+++ client/src/Utils/Utils.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const baseURL = "http://localhost:8086"
+const baseURL = process.env.API_URL||"http://localhost:8086"
 const API = axios.create({baseURL:baseURL})
 
 export{axios,API,baseURL}
